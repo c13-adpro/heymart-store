47542a5c08a3bb212a76ce21cc4a58539f635c05
juankhusuma <juan.d.khusuma@gmail.com>
2024-04-12 23:11:18 +0700

    [Hotfix] Made some tweak on default SpringBootTest

--- src/test/java/id/ac/ui/cs/advprog/heymartstore/HeymartStoreApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartstore/HeymartStoreApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.heymartstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HeymartStoreApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HeymartStoreApplicationTests {
 
     @Test
