363aacf0a34b5a3a8387473d8817b6717e7bac65
juankhusuma <juan.d.khusuma@gmail.com>
2024-04-12 23:03:26 +0700

    Project initialization

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartstore/HeymartStoreApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.heymartstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeymartStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeymartStoreApplication.class, args);
+    }
+
+}
